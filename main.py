e7eae3498e4761be7791758fd621664416a6c37d
Denis Diewold <d.diewold@gmx.net>
2018-04-17 00:13:10 +0200

    little better

--- main.py
+++ main.py
@@ -9,26 +9,26 @@ def search_actor(search_word):
     lower(name) % lower(\'{0}\') OR name ILIKE \'%{0}%\' OR name @@ \'{0} \'
     ORDER BY levenshtein(lower(\'{0}\'),
     lower(name));'''.format(search_word.get())
-    create_window(query)
+    create_window(query,' ' )
 
 def search_movie(search_word):
     query='''SELECT title FROM movies WHERE lower(title) % lower(\'{0}\') OR
     lower(title) % lower(\'{0}\') OR title ILIKE \'%{0}%\' OR title @@ \'{0} \'
     ORDER BY levenshtein(lower(\'{0}\'),
     lower(title));'''.format(search_word.get())
-    create_window(query)
+    create_window(query,' ' )
 
 def search_near_actor(search_word):
     query='''SELECT name FROM actors WHERE metaphone(name, 6) =
     metaphone(\'{0}\', 6) OR metaphone(name, 6) % metaphone(\'{0}\', 6)
     ;'''.format(search_word.get())
-    create_window(query)
+    create_window(query,' ' )
 
 def search_near_movie(search_word):
     query='''SELECT title FROM movies WHERE metaphone(title, 6) =
     metaphone(\'{0}\', 6) OR metaphone(title, 6) % metaphone(\'{0}\',
     6);'''.format(search_word.get())
-    create_window(query)
+    create_window(query,' ' )
 
 def search_all_movie(search_word):
     query='''SELECT name, title FROM actors NATURAL JOIN movies_actors NATURAL
@@ -36,7 +36,7 @@ def search_all_movie(search_word):
     lower(\'{0}\') OR lower(name) % lower(\'{0}\') OR name ILIKE \'%{0}%\' OR
     name @@ \'{0} \' ORDER BY levenshtein(lower(\'{0}\'), lower(name))) ORDER
     BY name, title;'''.format(search_word.get())
-    create_window(query)
+    create_window(query, ' in ')
 
 def search_all_actors(search_word):
     query='''SELECT name, title FROM actors NATURAL JOIN movies_actors NATURAL
@@ -45,22 +45,24 @@ def search_all_actors(search_word):
     \'%{0}%\' OR
     title @@ \'{0} \' ORDER BY levenshtein(lower(\'{0}\'), lower(title))) ORDER
     BY title, name;'''.format(search_word.get())
-    create_window(query)
+    create_window(query, ' in ')
 
-def create_window(query):
+def create_window(query, result_columns):
     cur.execute(query)
     result = cur.fetchall()
 
     toplevel = Toplevel()
     toplevel.title('Result')
+    toplevel.resizable(height=True, width=True)
     scrollbar = Scrollbar(toplevel)
     scrollbar.pack(side=RIGHT, fill =Y)
     
-    mylist = Listbox(toplevel, yscrollcommand = scrollbar.set, width=30)
+    mylist = Listbox(toplevel, yscrollcommand = scrollbar.set)
+    mylist.config(width=0)
 
     for item in result:
-        mylist.insert(END, '\t '.join(item))
-    mylist.pack( side=LEFT, fill = BOTH)
+        mylist.insert(END, result_columns.join(item))
+    mylist.pack()
     scrollbar.config(command = mylist.yview)
     toplevel.focus_set()
 
